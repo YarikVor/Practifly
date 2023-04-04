f16a2193b1a8484de58cc18666055ffcc2078155
Yaroslav Vorobyov <yarikhelov@gmail.com>
2023-04-04 21:46:07 +0300

    Added string connection

---
+++ PractiFly.WebApi/EntityDb/Users/GroupCourse.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PractiFly.WebApi.EntityDb.Users
+{
+    [Table("GroupCourse")]
+    public class GroupCourse
+    {
+        [Column("GroupId")]
+        public int GroupId { get; set; }
+
+        [Column("CourseId")]
+        public int CourseId { get; set; }
+
+        [Column("LevelId")]
+        public int LevelId { get; set; }
+
+        [Column("IsCompleted")]
+        public bool IsCompleted { get; set; }
+
+        [Column("Note")]
+        [MaxLength(256)]
+        public string? Note { get; set; }
+
+    }
+}
--- PractiFly.WebApi/EntityDb/Users/User.cs
+++ PractiFly.WebApi/EntityDb/Users/User.cs
@@ -4,44 +4,53 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 
 namespace PractiFly.WebApi.EntityDb.Users;
-/*
- *          Id // IdentityUserId
-        	FirstName
-        	LastName
-        	Email
-        	Phone
-        	FilePhoto //?
-        	RegistrationDate
-        	Note
-*/
-[Table("Users")]
+
+[Table("User")]
 [PrimaryKey("Id")]
-public class Users
+public class User
 {
     [Key]
     [Column("Id")]
     public int Id { get; set; }
+
     [Column("FirstName")]
+    [MaxLength(128)]
+    [Required]
     [MaybeNull]
     public string FirstName { get; set; }
+
     [Column("LastName")]
+    [MaxLength(128)]
+    [Required]
     [MaybeNull]
     public string LastName { get; set; }
+
     [EmailAddress]
     [Column("Email")]
+    [MaxLength(64)]
+    [Required]
     [MaybeNull]
     public string Email { get; set; }
+
     [Phone]
     [Column("Phone")]
+    [MaxLength(32)]
+    [Required]
     [MaybeNull]
     public string Phone { get; set; }
+
     [Url]
     [Column("FilePhoto")]
+    [MaxLength(64)]
+    [Required]
     [MaybeNull]
     public string FilePhoto { get; set; }
+
     [Column("RegistrationDate")]
     [MaybeNull]
     public DateOnly RegistrationDate { get; set; }
+
     [Column("Note")]
+    [MaxLength(256)]
     public string? Note { get; set; }
 }
