11e823471c57fa90b97331e128aed4a3f5147b34
SvValeron <valeron080403@gmail.com>
2023-04-04 23:06:34 +0300

    Merge backend with backend_Course

--- PractiFly.WebApi/EntityDb/Materials/Level.cs
+++ PractiFly.WebApi/EntityDb/Materials/Level.cs
@@ -1,4 +1,6 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
 
 namespace PractiFly.WebApi.EntityDb.Materials;
@@ -12,8 +14,9 @@ namespace PractiFly.WebApi.EntityDb.Materials;
  */
 
 
-[PrimaryKey("Id")]
+
 [Table("Level")]
+[PrimaryKey("Id")]
 public class Level
 {
     
