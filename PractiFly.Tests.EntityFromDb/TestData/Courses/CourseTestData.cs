610bb683e9fd2372b1a3c3847cdd325cc83a4554
YarikVor <yarikhelow@gmail.com>
2023-04-05 22:39:17 +0300

    Change context

--- PractiFly.Tests.EntityFromDb/TestData/Courses/CourseTestData.cs
+++ PractiFly.Tests.EntityFromDb/TestData/Courses/CourseTestData.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PractiFly.WebApi.Context;
+using PractiFly.WebApi.EntityDb.Users;
 
 namespace PractiFly.Tests.EntityFromDb.TestData.Courses
 {
     internal class Course
     {
+        private static IUsersContext _usersContext;
+
+        public void AddUser()
+        {
+            var user = new User[]
+            {
+                new()
+                {
+                    Id = 1,
+                    FirstName = "Test",
+                    LastName = "Test",
+                    Email = "",
+                    Phone = "",
+                    FilePhoto = "",
+                    RegistrationDate = DateOnly.FromDateTime(DateTime.Now),
+                },
+                // Add one
+            };
+            
+            _usersContext.Users.AddRange(user);
+        }
+        
     }
 }
