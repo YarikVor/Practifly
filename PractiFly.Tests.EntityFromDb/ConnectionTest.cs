99a7cf069a069493a3356dcf98505e5a3a3431d5
YarikVor <yarikhelow@gmail.com>
2023-04-05 19:57:05 +0300

    Changed tests

--- PractiFly.Tests.EntityFromDb/ConnectionTest.cs
+++ PractiFly.Tests.EntityFromDb/ConnectionTest.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using PractiFly.WebApi.Context;
 using Xunit.Abstractions;
+using PractiFly.WebApi.EntityDb.Users;
+using Xunit.Abstractions;
 
 namespace PractiFly.Tests.EntityFromDb;
 
@@ -16,6 +18,19 @@ public class ConnectionTest
     [Fact]
     public async Task TestConnection_Success()
     {
+        _usersContext.Update(new User
+        {
+            //Id = 1,
+            FirstName = "Yarik",
+            LastName = "Vorobyov",
+            Email = "yarikhelov@gmail.com",
+            Phone = "+380501234567",
+            FilePhoto = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+            RegistrationDate = new(2021, 1, 1),
+            Note = null
+        });
+        _usersContext.SaveChanges();
+        
         await _usersContext.Database.OpenConnectionAsync();
         await _usersContext.Database.CloseConnectionAsync();
     }
