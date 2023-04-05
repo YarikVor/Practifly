5e87e36a8dac4f586cc759701d71d175b6d35f43
YarikVor <yarikhelow@gmail.com>
2023-04-05 20:18:21 +0300

    Added attributes for Unit

--- PractiFly.WebApi/Db/Entity/Materials/Unit.cs
+++ PractiFly.WebApi/Db/Entity/Materials/Unit.cs
@@ -1,23 +1,28 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
 
 namespace PractiFly.WebApi.EntityDb.Materials
 {
     [Table("Unit")]
+    [Keyless]
     public class Unit
     {
         [Column("MaterialId")]
         public int MaterialId { get; set; }
+        
+        [ForeignKey("MaterialId")]
+        public virtual Material Material { get; set; }
 
         [Column("Text")]
-        [MaybeNull]
-        public string Text { get; set; }
+        [Required]
+        [MaxLength(2048)]
+        public string Text { get; set; } = null!;
 
         [Column("URL")]
         [MaxLength(2048)]
         [Required]
-        [MaybeNull]
-        public string Url { get; set; }
+        public string Url { get; set; } = null!;
     }
 }
