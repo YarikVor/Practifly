d6f25abb7690b8b82c6ea9446157314801fa1623
Vadym Manchenko <clashstarset2019@gmail.com>
2023-04-05 21:06:53 +0300

    Change Unit

--- PractiFly.WebApi/Db/Entity/Materials/Unit.cs
+++ PractiFly.WebApi/Db/Entity/Materials/Unit.cs
@@ -1,14 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 
 namespace PractiFly.WebApi.EntityDb.Materials
 {
     [Table("Unit")]
+    [Keyless]
     public class Unit
     {
         [Column("MaterialId")]
         public int MaterialId { get; set; }
+
+        [ForeignKey("MaterialKey")] 
+        public virtual Material Material { get; set; } = null!;
 
         [Column("Text")]
         [MaybeNull]
