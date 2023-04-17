65a73518a5bc5e3394f420b5168b28c002cf7bdd
Vadym Manchenko <clashstarset2019@gmail.com>
2023-04-17 17:14:23 +0300

    Remove files

--- PractiFly.WebApi/Dto/Admin/UserView/UserFullnameItemDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PractiFly.WebApi.Dto.Admin.UserView
-{
-    public class UserFullnameItemDto
-    {
-        public int Id { get; set; }
-
-        public string Fullname { get; set; } = null!; 
-    }
-}
