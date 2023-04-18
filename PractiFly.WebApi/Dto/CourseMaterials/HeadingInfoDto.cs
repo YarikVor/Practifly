ce5634bd6992184960a2da7673a997374a68ac63
Vadym Manchenko <clashstarset2019@gmail.com>
2023-04-18 14:00:51 +0300

    Add dto for CourseMaterials

---
+++ PractiFly.WebApi/Dto/CourseMaterials/HeadingInfoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PractiFly.WebApi.Dto.CourseMaterials
+{
+    public class HeadingInfoDto
+    {
+        public int Id { get; set; }
+        public string HeadingItem { get; set; } = null!;
+    }
+}
