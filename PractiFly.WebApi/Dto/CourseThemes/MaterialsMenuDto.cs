dd0e7418c8715567c6ee31450ca9078ef297d3e8
Vadym Manchenko <clashstarset2019@gmail.com>
2023-04-21 13:39:47 +0300

    Create MaterialsMenuDto

---
+++ PractiFly.WebApi/Dto/CourseThemes/MaterialsMenuDto.cs
@@ -0,0 +1,11 @@
+﻿namespace PractiFly.WebApi.Dto.CourseThemes
+{
+    public class MaterialsMenuDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int Grade { get; set; }
+        public bool IsSelected { get; set; }
+
+    }
+}
