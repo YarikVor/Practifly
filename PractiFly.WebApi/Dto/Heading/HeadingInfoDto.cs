94ab5eec9e7d5ec1790cffee87173a727a238f94
YarikVor <yarikhelov@gmail.com>
2023-04-27 10:35:44 +0300

    Add controller for CourseHeading

--- PractiFly.WebApi/Dto/Heading/HeadingInfoDto.cs
+++ PractiFly.WebApi/Dto/Heading/HeadingInfoDto.cs
@@ -1,25 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace PractiFly.WebApi.Dto.Heading
+namespace PractiFly.WebApi.Dto.Heading;
+
+public class HeadingInfoDto
 {
-    public class HeadingInfoDto
-    {
-        [Required]
-        [MaxLength(128)]
-        public string Name { get; set; } = null!;
+    [Required]
+    [MaxLength(128)]
+    public string Name { get; set; } = null!;
 
-        [Required]
-        [MaxLength(16)]
-        public string Code { get; set; } = null!;
+    [Required]
+    [MaxLength(16)]
+    public string Code { get; set; } = null!;
 
-        [Required]
-        [MaxLength(16)]
-        public string Udc { get; set; } = null!;
+    [Required]
+    [MaxLength(16)]
+    public string Udc { get; set; } = null!;
 
-        [MaxLength(256)]
-        public string? Note { get; set; }
+    [MaxLength(256)]
+    public string? Note { get; set; }
 
-        [MaxLength(65536)]
-        public string? Description { get; set; }
-    }
+    [MaxLength(65536)]
+    public string? Description { get; set; }
 }
--- PractiFly.WebApi/Dto/Heading/HeadingItemDto.cs
+++ PractiFly.WebApi/Dto/Heading/HeadingItemDto.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
-        public int Code { get; set; }
+        public string Code { get; set; } = null!;
     }
 }
+
+
