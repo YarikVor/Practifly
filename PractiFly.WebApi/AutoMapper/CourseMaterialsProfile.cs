e8d5ce74031e8f9c086495037f18d2562f5f20a5
SvValeron <valeron080403@gmail.com>
2023-04-28 23:02:58 +0300

    Edit Dto CourseMaterialsProfile

--- PractiFly.WebApi/AutoMapper/CourseMaterialsProfile.cs
+++ PractiFly.WebApi/AutoMapper/CourseMaterialsProfile.cs
@@ -11,7 +11,7 @@ public class CourseMaterialsProfile : Profile
     public CourseMaterialsProfile(IPractiflyContext _context)
     {
         CreateProjection<Heading, HeadingInfoDto>();
-        CreateProjection<Material, MaterialBlocksDto>() //Id, Name, IsPractical
+        CreateProjection<Material, MaterialForInclusionDto>() //Id, Name, IsPractical
             .ForMember(dto => dto.IsIncluded, par => par.MapFrom(
                 e => _context
                     .CourseMaterials
@@ -20,16 +20,13 @@ public class CourseMaterialsProfile : Profile
                 e => _context
                     .CourseMaterials
                     .Where(cm => cm.MaterialId == e.Id)
-                    .Select(cm => cm.PriorityLevel))) //Чи правильно?
-            //.ForMember(dto => dto.Type, par => par.MapFrom(
-            //    )
-            ;
-        CreateProjection<Material, MaterialFromIncludedBlockViewDto>()
-            .ForMember(dto => dto.PriorityLevel, par => par.MapFrom(
-                e => _context
-                    .CourseMaterials
-                    .Where(cm => cm.MaterialId == e.Id)
                     .Select(cm => cm.PriorityLevel)));
+        //CreateProjection<Material, MaterialFromIncludedBlockViewDto>()
+        //    .ForMember(dto => dto.PriorityLevel, par => par.MapFrom(
+        //        e => _context
+        //            .CourseMaterials
+        //            .Where(cm => cm.MaterialId == e.Id)
+        //            .Select(cm => cm.PriorityLevel)));
         //CreateProjection<ThemeMaterial, ThemeMaterialInfoDto>()
         //    .ForMember(dto => dto.Material, par => par.MapFrom(
         //        tm =>  tm.Material ))
