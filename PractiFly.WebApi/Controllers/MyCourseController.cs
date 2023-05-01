39230e4e6940c6c0f47c9247feaf243d8da486bf
SvValeron <valeron080403@gmail.com>
2023-05-01 23:02:01 +0300

    Change method UserCourse for class MyCourseController

--- PractiFly.WebApi/Controllers/MyCourseController.cs
+++ PractiFly.WebApi/Controllers/MyCourseController.cs
@@ -1,10 +1,14 @@
 ﻿using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PractiFly.DbContextUtility.Context.PractiflyDb;
+using PractiFly.DbEntities.Users;
 using PractiFly.WebApi.Dto.MyCourse;
+using System.Security.Claims;
 
 namespace PractiFly.WebApi.Controllers
 {
@@ -14,11 +18,13 @@ namespace PractiFly.WebApi.Controllers
     {
         private readonly IPractiflyContext _context;
         private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
 
-        public MyCourseController(IPractiflyContext context, IMapper mapper)
+        public MyCourseController(IPractiflyContext context, IMapper mapper, UserManager<User> userManager)
         {
             _context = context;
             _mapper = mapper;
+            _userManager = userManager;
         }
 
         /// <summary>
@@ -29,14 +35,28 @@ namespace PractiFly.WebApi.Controllers
         /// <response code="400">Operation was failed.</response>
         /// <response code="404">No courses found.</response>
         /// <returns>A JSON-encoded representation of the list of courses and their statuses.</returns>
+        [Authorize(AuthenticationSchemes = "Bearer")]
         [HttpGet]
         [Route("user/courses")]
-        public async Task<IActionResult> UserCourse(int userId)
+        public async Task<IActionResult> UserCourse()
         {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (userId == null)
+            {
+                return BadRequest();
+            }
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                // Якщо користувача з таким ідентифікатором не знайдено, видається помилка.
+                return BadRequest();
+            }
+            int userIntId = user.Id;
             UserCourseStatusDto[] result = await _context
                 .UserCourses
                 .AsNoTracking()
-                .Where(e => e.UserId == userId)
+                .Where(e => e.UserId == userIntId)
                 .ProjectTo<UserCourseStatusDto>(_mapper.ConfigurationProvider)
                 .ToArrayAsync();
 
