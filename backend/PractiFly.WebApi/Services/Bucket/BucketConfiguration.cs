230bba86a2d9a5dbdffc1d77789c21255d037e5d
Yaroslav Vorobyov <yarikhelov@gmail.com>
2023-05-21 19:08:22 +0300

    Add BucketConfiguration

---
+++ backend/PractiFly.WebApi/Services/Bucket/BucketConfiguration.cs
@@ -0,0 +1,26 @@
+using PractiFly.WebApi.Services.AuthenticationOptions;
+
+namespace PractiFly.WebApi;
+//Bucket
+public class BucketConfiguration : ConfigurationAbstraction, IBucketConfiguration
+{
+    protected override string SectionName => "AWS";
+    
+    private const string AccessKeyName = "AccessKeyId";
+    private const string SecretKeyName = "SecretAccessKey";
+    private const string RegionKeyName = "Region";
+    private const string BucketKeyName = "BucketName";
+    private const string BaseUrlKeyName = "BaseUrl";
+    private const string ProfileKeyName = "Profile";
+
+    public BucketConfiguration(IConfiguration configuration) : base(configuration)
+    {
+    }
+
+    public string AccessKey => GetValue(AccessKeyName);
+    public string SecretKey => GetValue(SecretKeyName);
+    public string Region => GetValue(RegionKeyName);
+    public string BucketName => GetValue(BucketKeyName);
+    public string BaseUrl => GetValue(BaseUrlKeyName);
+    public string ProfileName => GetValue(ProfileKeyName);
+}
